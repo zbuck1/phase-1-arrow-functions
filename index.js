b47e69bf3825e0826f962eaa7cfc6bed27299c1c
Zoe <buckzoe5@gmail.com>
2022-01-17 14:53:32 -0500

    commit tree a8707ae305cc1714ae9a3d3755755651b4369e27

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = function(num1 = 2000, num2 = 100) {
+    return num1 / num2
+}
+
+const square = (x) => x * x;
+square(2);
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(3,4);
